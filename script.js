3771d9e512cc68f4dc78ed09b7d34da80d830f4c
Florin Pop <popflorin1705@yahoo.com>
2020-02-07 15:45:06 +0200

    init

---
+++ script.js
@@ -0,0 +1,397 @@
+const channel = {
+	name: 'Florin Pop',
+	url: '/c/FlorinPop',
+	logo:
+		'https://lh3.googleusercontent.com/a-/AAuE7mCNtBMEoAp9OtutSXdEMbAaKdI39S6BcKfVMD0k=s88-c-k-c0x00ffffff-no-rj-mo'
+};
+
+const videos = [
+	{
+		url: 'https://www.youtube.com/watch?v=ydZc17rlR5E&t=166s',
+		title:
+			'Responsive Navigation with Hamburger Menu - CSS And JavaScript Tutorial',
+		thumb:
+			'https://i.ytimg.com/vi/ydZc17rlR5E/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAlM5P5o8Z41ggOZEd8hQreS8zqFQ'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=cX0qFc1wSkE&t=167s',
+		title: 'Live Stream - Planning Future Content',
+		thumb:
+			'https://i.ytimg.com/vi/cX0qFc1wSkE/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDNkylcmBLFmN_yYamWEol2IDlANg'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=PNcg5oPt9Gc&t=2s',
+		title: 'Animated Sidebar Menu - CSS and JavaScript tutorial - Day 31',
+		thumb:
+			'https://i.ytimg.com/vi/PNcg5oPt9Gc/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCkB31bXt4gY8cv_Ml9H232WwVEcw'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=UttNoIzfxS8&t=2s',
+		title: 'Zoom Effect - CSS and JavaScript tutorial - Day 30',
+		thumb:
+			'https://i.ytimg.com/vi/UttNoIzfxS8/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA4MxLfyF_Pmkf8nozcuYttRGQ-tw'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=7b4BwdjrxdU&t=15s',
+		title: 'Decoding Binary Message with JavaScript - Day 29',
+		thumb:
+			'https://i.ytimg.com/vi/7b4BwdjrxdU/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA7acNBaNT7HNO7f14ErHJmx2OF5A'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=VR4B4eOETEg&t=20s',
+		title: 'CSS Triangle Shape - Day 28',
+		thumb:
+			'https://i.ytimg.com/vi/VR4B4eOETEg/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAe_UMDIItt-9Fy6rSxvt3cIBueSQ'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=7bJAwHBVkpQ&t=306s',
+		title: 'Wave Animation on inputs - CSS and JavaScript Tutorial - Day 27',
+		thumb:
+			'https://i.ytimg.com/vi/7bJAwHBVkpQ/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD7O0Vod4kFNN_dyD2-EhzSBIuDPQ'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=v_Za9mytBoI&t=1304s',
+		title: 'Live Stream - Building a JavaScript Live Trivia Game - Part 1',
+		thumb:
+			'https://i.ytimg.com/vi/v_Za9mytBoI/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD75R4HMmSY7Cpm-kAuTuIyjbi5zQ'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=NskWPRF3C2s&t=45s',
+		title: 'Rotating Circles Animation - Day 26',
+		thumb:
+			'https://i.ytimg.com/vi/NskWPRF3C2s/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA0-T_ilGVGpzjBQ3KC3UxrPimadw'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=klR2BdgQBWg&t=1s',
+		title: 'HTML and CSS Newsletter Design - Day 25',
+		thumb:
+			'https://i.ytimg.com/vi/klR2BdgQBWg/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDZ-HP2MCs2YnuqLdllfdeqbJsxmA'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=xb0w_DmWtVs',
+		title: 'Kinetic Loader Animation with CSS - Day 24',
+		thumb:
+			'https://i.ytimg.com/vi/xb0w_DmWtVs/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCSo4YD46c8jpZZkw7-LHinOvOI_w'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=zCpOq9b8XIs&t=2s',
+		title: 'Website Automatic Deploy with @GitHub and @Netlify - Day 23',
+		thumb:
+			'https://i.ytimg.com/vi/zCpOq9b8XIs/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLB_8y5yRWFn_qyjfiLXK6hO5mW7Lw'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=b9gn8SbGPfk&t=1s',
+		title:
+			'Live Stream - Coding a Portfolio Website from scratch with Creative Tim',
+		thumb:
+			'https://i.ytimg.com/vi/b9gn8SbGPfk/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAWfsgFJM3zv1MXbq5PsmSUBFgyqA'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=IeMZYNpxnII&t=4s',
+		title: 'How to add a Countdown Timer in OBS - Day 22',
+		thumb:
+			'https://i.ytimg.com/vi/IeMZYNpxnII/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCgBNli5zSQXNp9P-uTzt3Dy6erZQ'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=XBRpIbo5JEk&t=20s',
+		title:
+			'Live Stream - Solving Coding Challenges with JavaScript on CodeWars',
+		thumb:
+			'https://i.ytimg.com/vi/XBRpIbo5JEk/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDF-E37BbU6r9afxquwdLPWpCbnQQ'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=x7WJEmxNlEs&t=14s',
+		title: 'Simple Countdown Timer with JavaScript - Day 21',
+		thumb:
+			'https://i.ytimg.com/vi/x7WJEmxNlEs/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCIFbnLlFfxF9p1YSze36zmpWvQLg'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=tRjlY-NobZY',
+		title: 'Live Stream - Building a ReactJS App from Scratch',
+		thumb:
+			'https://i.ytimg.com/vi/tRjlY-NobZY/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAM9YdXm5aBNZnd4vGox91NsYxPPQ'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=P_9GxIjrbLY',
+		title: 'FREE and Fast Website Hosting using @Netlify - Day 20',
+		thumb:
+			'https://i.ytimg.com/vi/P_9GxIjrbLY/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDm0o4uzJnRkR0aGXj40vE6e_elwg'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=uIj8wbvxZIk',
+		title: 'Live Stream - Coding a web app (Countries REST) from scratch',
+		thumb:
+			'https://i.ytimg.com/vi/uIj8wbvxZIk/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjOkXt3K8DfuD1BNd-ODPAmlvr0Q'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=XL68br6JyYs&t=10s',
+		title: 'PokeDex - JavaScript Tutorial - Day 19',
+		thumb:
+			'https://i.ytimg.com/vi/XL68br6JyYs/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDxv0MHX4DsWT0OFKrbdE95kkjy5w'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=gotRbtnJhxM',
+		title:
+			'Live Stream - Planning Next Week Live Streams + Coded a Random Image Generator',
+		thumb:
+			'https://i.ytimg.com/vi/gotRbtnJhxM/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC2RDgPVuz_hn_0bzw8cncj_W19zA'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=W6mx0huuo3g',
+		title: 'Waves Animation with JavaScript - Day 18',
+		thumb:
+			'https://i.ytimg.com/vi/W6mx0huuo3g/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBlp7s6xkVSM7vdw3KOjoxDfHO7tA'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=R8GS-8nlekY&t=1s',
+		title: 'Live Visits Counter - JavaScript tutorial - Day 17',
+		thumb:
+			'https://i.ytimg.com/vi/R8GS-8nlekY/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAvOOcMBHEbY_wE1r7FZCn200cDzQ'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=nYzrWnwpw34&t=1s',
+		title: 'Random Meal Generator - JavaScript tutorial - Day 16',
+		thumb:
+			'https://i.ytimg.com/vi/nYzrWnwpw34/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAAXOAvfspLVc9gkM7-R1euAPakxw'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=AbRgaY0khPM&t=1s',
+		title: 'React Progress Bar - with hooks - Day 15',
+		thumb:
+			'https://i.ytimg.com/vi/AbRgaY0khPM/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD0H1EWsWG3tbArNgbJGM7ocEc6Rw'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=BpU_ajZ_lpw&t=17s',
+		title: 'CSS Smooth Scroll - Only one line - Day 14',
+		thumb:
+			'https://i.ytimg.com/vi/BpU_ajZ_lpw/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBpEPptk-wolz-LZi0NxLt6b82ZwA'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=qyLoi2jLNZM&t=17s',
+		title: 'Auto Text Effect - JavaScript - Day 13',
+		thumb:
+			'https://i.ytimg.com/vi/qyLoi2jLNZM/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDjHZwBRkl9X6kr10jFgr1MAwpMAA'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=069NlLdB4dM&t=299s',
+		title: 'Live Stream - Coding a Landing Page from scratch',
+		thumb:
+			'https://i.ytimg.com/vi/069NlLdB4dM/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBoVnMN5P7u8Q3Ap2uG-LAHAF-58Q'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=zi7FRJHIlvc',
+		title: 'Mix Blend Loader - CSS Tutorial - Day 12',
+		thumb:
+			'https://i.ytimg.com/vi/zi7FRJHIlvc/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBcnSYTWFjhcTKX-TgUDlr6CGtH4A'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=5pdD2lmrSuA',
+		title: 'Live Stream - JavaScript Coding Challenges + Chill stream',
+		thumb:
+			'https://i.ytimg.com/vi/5pdD2lmrSuA/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD0rWT0tOKrf5ruAESbJRMrmL9ejQ'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=PjeLijYIiCA&t=253s',
+		title: 'Hover Board - Fun with JavaScript - Day 11',
+		thumb:
+			'https://i.ytimg.com/vi/PjeLijYIiCA/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDZRCLadvksefFgLGf9HUWAktGVsA'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=r-LI74QNF34&t=58s',
+		title: 'Image Reflection - CSS effect - Day 10',
+		thumb:
+			'https://i.ytimg.com/vi/r-LI74QNF34/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBOP8nnvuW1qUHo_8GOl7iyNkdCzw'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=aKJwLN40CPs',
+		title: 'Traffic Lights Simulator with CSS and JavaScript - Day 09',
+		thumb:
+			'https://i.ytimg.com/vi/aKJwLN40CPs/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBizqkGve9CT2mniok3nq_2HQtISg'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=IglwkruelAw&t=2s',
+		title: 'CSS Custom Checkbox - Day 08',
+		thumb:
+			'https://i.ytimg.com/vi/IglwkruelAw/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCpyyjMra-3rO6arQ1qL50lyCVp0w'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=KTIrFsnbaGA',
+		title: 'Live Stream - Coding 3 CSS and JS Animation Projects 🤩',
+		thumb:
+			'https://i.ytimg.com/vi/KTIrFsnbaGA/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCCkAXUS2XGUe71mwJUzrFkMxJueA'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=RoFmYay_waA&t=47s',
+		title: 'JavaScript Visual Counter - Day 07',
+		thumb:
+			'https://i.ytimg.com/vi/RoFmYay_waA/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCwiAtfCOAUhdeE9nwEkR6KA2qUtQ'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=tcUVUBlyXX8&t=1s',
+		title: 'Timeline Component in React (and some CSS Magic 🎩) - Day 06',
+		thumb:
+			'https://i.ytimg.com/vi/tcUVUBlyXX8/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBijxzB71MzzsIP5D375PVEkAXOmg'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=ad-lVe-0zpw&t=23s',
+		title: "Live Stream - Coding Session - We're building 2 projects 🤩",
+		thumb:
+			'https://i.ytimg.com/vi/ad-lVe-0zpw/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBahWvE8om0G7T5bY0ExwTd-V7YAg'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=Hl52Q6GV4Sk&t=20s',
+		title: 'Line Through Effect - CSS and JavaScript - Day 05',
+		thumb:
+			'https://i.ytimg.com/vi/Hl52Q6GV4Sk/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBVY7JIhuYczz73vN7YXy6bTBKuLQ'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=gUslncDB7Ds',
+		title: 'Live stream - Stream setup, Q&A',
+		thumb:
+			'https://i.ytimg.com/vi/gUslncDB7Ds/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLALWJlUDIaQgOnNwHO-dGkJbjx-AA'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=Ga5dOwAQj0o&t=50s',
+		title: 'Image Password Strength Effect - JavaScript tutorial - Day 04',
+		thumb:
+			'https://i.ytimg.com/vi/Ga5dOwAQj0o/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAiQNFLzUv9EqGzhdf8sggruOKU0w'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=qMf8KCOwnuc&t=3s',
+		title:
+			"Dad Jokes Generator with JavaScript - Don't Laugh Challenge - Day 03",
+		thumb:
+			'https://i.ytimg.com/vi/qMf8KCOwnuc/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD70FKuJyceAWSNFrmx4NL7YjzH-g'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=U-4tg5snUuw&t=12s',
+		title: 'Custom Progress Bar with CSS and JavaScript - Day 02',
+		thumb:
+			'https://i.ytimg.com/vi/U-4tg5snUuw/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDd3JIQoruZEOp-Y0IC26iYTnc_6A'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=5ElUDN2It0E&t=2s',
+		title: 'Welcome 2020 🤩 - #31Days31Videos Intro - Day 01',
+		thumb:
+			'https://i.ytimg.com/vi/5ElUDN2It0E/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC_zcV63ZrX_yxzUuc0VIvuZllwJA'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=FbCcXosc3U4&t=2675s',
+		title: 'Live stream - 2019 Overview & 2020 Plans + Q&A',
+		thumb:
+			'https://i.ytimg.com/vi/FbCcXosc3U4/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBQTbXXLm8Vtu5UwfhzTKD9VZfd7A'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=_ARGxz_cU_o',
+		title: '❄ Make it snow ❄ - JavaScript DOM Tutorial',
+		thumb:
+			'https://i.ytimg.com/vi/_ARGxz_cU_o/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDdC48zXFIDzks4UweKQPYws7JqoA'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=zSHCi12hz9o&t=16s',
+		title: 'CSS3 Pulse Effect',
+		thumb:
+			'https://i.ytimg.com/vi/zSHCi12hz9o/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLChGtye5RYs7DqJ8VR_oO5OHFxvvg'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=taxj6Vmt0Hk&t=1s',
+		title: 'JavaScript Random Picker Visualizer & 🔥 1000 Subs Giveaway 🔥',
+		thumb:
+			'https://i.ytimg.com/vi/taxj6Vmt0Hk/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDbY6mCynzPhLmqz5T6eyFan6e86w'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=twpxoIX59Bo&t=40s',
+		title: 'Numerical Palindrome #1 - JavaScript Coding Challenge',
+		thumb:
+			'https://i.ytimg.com/vi/twpxoIX59Bo/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLB4CfGLJuFcDw3abvH6JSDSwdOhRQ'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=rsd4FNGTRBw&t=123s',
+		title: 'JavaScript Client-side Form Validation',
+		thumb:
+			'https://i.ytimg.com/vi/rsd4FNGTRBw/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAnx9OyKFOvpDBIue2IlcGxz2ItHA'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=bnafrV1CZxY',
+		title: 'Live Coding project #074 of #100Days100Projects - Stream #001',
+		thumb:
+			'https://i.ytimg.com/vi/bnafrV1CZxY/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAb2HZ9ezJZ4Rp8qCJ0J4HkNnDc6g'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=rNOscFG0PYo',
+		title: 'Netflix Mobile Navigation Animation with CSS and JavaScript',
+		thumb:
+			'https://i.ytimg.com/vi/rNOscFG0PYo/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDSXF2yMzpox0Kry5Awba9Bl0dQaQ'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=L8X4zAsoxb4',
+		title: 'Infinite Scrolling with JavaScript',
+		thumb:
+			'https://i.ytimg.com/vi/L8X4zAsoxb4/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCu0LEqj4ctVLcm3sALYf2I6loFwQ'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=oFafzadAhn0&t=68s',
+		title: 'JavaScript Event KeyCode Previewer',
+		thumb:
+			'https://i.ytimg.com/vi/oFafzadAhn0/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCeEYisXyjs0UIQ2PnhxzreIHxf4w'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=xodD0nw2veQ&t=17s',
+		title: 'Light/Dark Theme Toggle with CSS and JavaScript',
+		thumb:
+			'https://i.ytimg.com/vi/xodD0nw2veQ/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBmW_JXJCGEnH5Q5vym4KfaDHEEBw'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=7f2HNadULOs&t=89s',
+		title: 'JavaScript Fetch API - Simple Example',
+		thumb:
+			'https://i.ytimg.com/vi/7f2HNadULOs/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBasnyTnS25n3liyeeVDNDKQ47F2Q'
+	},
+	{
+		url: 'https://www.youtube.com/watch?v=NpPrEWdnofU&t=59s',
+		title: 'Jumping Loading Animation with CSS',
+		thumb:
+			'https://i.ytimg.com/vi/NpPrEWdnofU/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDOG-eh-k972ls44yPDmrZdYFd1Bg'
+	}
+];
+
+const contents = document.getElementById('contents');
+
+function createCard(video) {
+	const youtubeCard = document.createElement('div');
+	youtubeCard.classList.add('youtube-card');
+
+	youtubeCard.innerHTML = `
+        <a class="thumb-container" href="${video.url}">
+            <img
+                src="${video.thumb}"
+                alt="thumb"
+            />
+        </a>
+        <div class="card-info">
+            <a class="card-info-left" href="${channel.url}">
+                <img
+                    src="${channel.logo}"
+                    alt="user"
+                />
+            </a>
+            <div class="card-info-right">
+                <a class="title" href="${video.url}">
+                    ${video.title}
+                </a>
+                <a class="user" href="${channel.url}">
+                    ${channel.name}
+                </a>
+            </div>
+        </div>
+    `;
+
+	contents.prepend(youtubeCard);
+}
+
+function getRandomVideo() {
+	return videos[Math.floor(Math.random() * videos.length)];
+}
+
+createCard(getRandomVideo());
+createCard(getRandomVideo());
